d56e759cfb11cd6b7e71ddce1b6dc892f1c255f9
Jorge Greñas <j4ck.devcode@gmail.com>
2021-02-17 17:15:06 -0500

    commented Provider

--- lib/provider/example_provider.dart
+++ lib/provider/example_provider.dart
@@ -1,21 +1,22 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
-class ExampleProvider {
+// class ExampleProvider {
 
-  final String _baseUrl = "firestore.googleapis.com";
+//   final String _baseUrl = "firestore.googleapis.com";
 
-  Future<dynamic> _processResp(Uri url) async {
-    final response = await http.get(url);
-    final decodedData = jsonDecode(response.body);
-    return decodedData;
-  }
+//   Future<dynamic> _processResp(Uri url) async {
+//     final response = await http.get(url);
+//     final decodedData = jsonDecode(response.body);
+//     return decodedData;
+//   }
 
-  Future<dynamic> getData() async {
-    final _url = Uri.https(_baseUrl, "/v1/projects/dart-server-example/databases/(default)/documents/i_dont_f_know");
-    final resp = await _processResp(_url);
-    return resp;
-  }
+//   Future<dynamic> getData() async {
+//     final _url = Uri.https(_baseUrl, "/v1/projects/dart-server-example/databases/(default)/documents/i_dont_f_know");
+//     print(_url);
+//     //final resp = await _processResp(_url);
+//     return _url;
+//   }
 
-}
+// }
